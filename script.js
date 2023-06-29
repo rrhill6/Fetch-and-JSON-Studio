efbf5e48ad64452ba6a1b82835a70376a503468e
Rochell Hill <rrhill6@yahoo.com>
2023-06-29 17:26:53 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 // TODO: add code here
+window.addEventListener("load", function() {
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
+        response.json().then( function(json) {
+            const div = document.getElementById("container");
+            // Add HTML that includes the JSON data
+            div.innerHTML = ""
+            for (index = 0; index < 8; index++) {
+                div.innerHTML += `
+                <div class = "astronaut">
+                 <h3>${json[index].firstName} ${json[index].lastName}</h3>
+                 <ul>
+                    <li>Hours in space: ${json[index].hoursInSpace}</li>
+                    <li>Active: ${json[index].active}</li>
+                     <li>Skills: ${json[index].skills}</li>
+                </ul>
+                <img class="avatar" src=${json[index].picture}>
+                </div>
+            `
+         }
+     });
+    });
+
+ } );
